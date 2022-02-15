75c0b23029c03992233d6d7812121aae2000a874
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-02-15 16:17:10 -0500

    Hero Tablet Complete/ Starting Skills Tablet

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,7 @@ export function Header() {
                 <h2 className="name">
                     <span>T</span>roy <span>R</span>ichards
                 </h2>
-                <h1> I type on keyboards and make web things look good.</h1>
+                <h1 className='hero-header'> I type on keyboards and make web things look good.</h1>
                 <p className='hero-desc'>
                     I am a Frontend Developer with experience building and designing responsive websites.
                     I am currently looking for a position as a Frontend Developer.
