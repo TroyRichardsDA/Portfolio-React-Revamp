4881c63e3b16583cf2ac3d57020bb431b6a34124
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-02-16 10:13:50 -0500

    Desktop Quries Complete.

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -3,21 +3,25 @@ export function Skills() {
   <section id="skills">
       <h2 className="skills-header"><i class="fas fa-tasks"></i>Skills</h2>
     <div className="skills-content">
-      <h3 className="skills-subheader">Developer Skills</h3>
-      <div class="devskills-wrap">
-        <p className="skill"><i className="fab fa-html5"></i> HTML <i className="fab fa-html5"></i></p>
-        <p className="skill"><i className="fab fa-css3"></i> CSS <i className="fab fa-css3"></i></p>
-        <p className="skill"><i class="fab fa-js"></i> JavaScript <i className="fab fa-js"></i></p>
-        <p className="skill"><i className="fab fa-react"></i>  React <i className="fab fa-react"></i> </p>
-        <p className="skill"><i className="fas fa-mobile-alt"></i> Responsive Web Development <i className="fas fa-laptop"></i></p>
+      <div className='skills-container'>
+        <h3 className="skills-subheader">Developer Skills</h3>
+        <div className="devskills-wrap">
+          <p className="skill"><i className="fab fa-html5"></i> HTML <i className="fab fa-html5"></i></p>
+          <p className="skill"><i className="fab fa-css3"></i> CSS <i className="fab fa-css3"></i></p>
+          <p className="skill"><i class="fab fa-js"></i> JavaScript <i className="fab fa-js"></i></p>
+          <p className="skill"><i className="fab fa-react"></i> React <i className="fab fa-react"></i> </p>
+          <p className="skill"><i className="fas fa-mobile-alt"></i> Responsive Web Development <i className="fas fa-laptop"></i></p>
+        </div>
       </div>
-      <h3 className="tool-subheader">Tools & Platforms</h3>
-      <div className="tools-wrap">
-        <p className="tool"><i className="fab fa-git-alt"></i> Git | GitHub <i className="fab fa-github-square"></i></p>
-        <p className="tool"><i className="fab fa-chrome"></i> Chrome DevTools <i className="fas fa-tools"></i></p>
-        <p className="tool"><i className="fab fa-codepen"></i> Codepen <i className="fab fa-codepen"></i></p>
-        <p className="tool"><i className="fas fa-code-branch"></i> Github Pages <i className="fas fa-code-branch"></i></p>
-        <p className="tool"><i className="far fa-image"></i> Adobe Photoshop <i className="fas fa-image"></i></p>
+      <div className="tool-container">
+        <h3 className="tool-subheader">Tools & Platforms</h3>
+        <div className="tools-wrap">
+          <p className="tool"><i className="fab fa-git-alt"></i> Git | GitHub <i className="fab fa-github-square"></i></p>
+          <p className="tool"><i className="fab fa-chrome"></i> Chrome DevTools <i className="fas fa-tools"></i></p>
+          <p className="tool"><i className="fab fa-codepen"></i> Codepen <i className="fab fa-codepen"></i></p>
+          <p className="tool"><i className="fas fa-code-branch"></i> Github Pages <i className="fas fa-code-branch"></i></p>
+          <p className="tool"><i className="far fa-image"></i> Adobe Photoshop <i className="fas fa-image"></i></p>
+        </div>
       </div>
     </div>
   </section> 
