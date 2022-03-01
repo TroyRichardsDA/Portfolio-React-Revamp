7f1384cbd0080b793c1828535a93d288d2e04d0e
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-03-01 18:41:36 -0500

    Skills Styled Comps Complete

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,65 +1,65 @@
 import { SiTailwindcss } from "react-icons/si";
-import { Container } from "./SkillsStyles";
+import {
+  Container,
+  Header,
+  DevSkills,
+  DevSkill,
+  Wrapper,
+} from "./SkillsStyles";
 
 export default function Skills() {
   return (
     <Container id="skills">
-      <h2 className="skills-header">
-        <i class="header-icon fas fa-tasks"></i>Skills
-      </h2>
-      <div className="skills-content">
-        <div className="skills-container">
-          <div className="devskills-wrap">
-            <p className="skill">
-              <i className="fab fa-html5"></i> HTML{" "}
-              <i className="fab fa-html5"></i>
-            </p>
-            <p className="skill">
-              <i className="fab fa-css3"></i> CSS{" "}
-              <i className="fab fa-css3"></i>
-            </p>
-            <p className="skill">
-              <i className="fab fa-js"></i> JavaScript{" "}
-              <i className="fab fa-js"></i>
-            </p>
-            <p className="skill">
-              <i className="fab fa-react"></i> React{" "}
-              <i className="fab fa-react"></i>{" "}
-            </p>
-            <p className="skill">
-              <SiTailwindcss /> Tailwind <SiTailwindcss />{" "}
-            </p>
-          </div>
-        </div>
-        <div className="tools-container">
-          <div className="tools-wrap">
-            <p className="tool">
-              <i className="fab fa-git-alt"></i> Git | GitHub{" "}
-              <i className="fab fa-github-square"></i>
-            </p>
-            <p className="tool">
-              <i className="fab fa-chrome"></i> Chrome DevTools{" "}
-              <i className="fas fa-tools"></i>
-            </p>
-            <p className="tool">
-              <i className="fab fa-codepen"></i> Codepen{" "}
-              <i className="fab fa-codepen"></i>
-            </p>
-            <p className="tool">
-              <i className="fas fa-code-branch"></i> Github Pages{" "}
-              <i className="fas fa-code-branch"></i>
-            </p>
-            <p className="tool">
-              <i className="far fa-image"></i> Adobe Photoshop{" "}
-              <i className="fas fa-image"></i>
-            </p>
-            <p className="tool">
-              <i className="fa-brands fa-figma"></i> Figma{" "}
-              <i className="fa-brands fa-figma"></i>
-            </p>
-          </div>
-        </div>
-      </div>
+      <Wrapper>
+        <Header className="skills-header">
+          <i class="header-icon fas fa-tasks"></i>Skills
+        </Header>
+        <DevSkills>
+          <DevSkill>
+            <i className="fab fa-html5"></i> HTML{" "}
+            <i className="fab fa-html5"></i>
+          </DevSkill>
+          <DevSkill>
+            <i className="fab fa-css3"></i> CSS <i className="fab fa-css3"></i>
+          </DevSkill>
+          <DevSkill>
+            <i className="fab fa-js"></i> JavaScript{" "}
+            <i className="fab fa-js"></i>
+          </DevSkill>
+          <DevSkill>
+            <i className="fab fa-react"></i> React{" "}
+            <i className="fab fa-react"></i>{" "}
+          </DevSkill>
+          <DevSkill>
+            <SiTailwindcss /> Tailwind <SiTailwindcss />{" "}
+          </DevSkill>
+
+          <DevSkill>
+            <i className="fab fa-git-alt"></i> Git | GitHub{" "}
+            <i className="fab fa-github-square"></i>
+          </DevSkill>
+          <DevSkill>
+            <i className="fab fa-chrome"></i> Chrome DevTools{" "}
+            <i className="fas fa-tools"></i>
+          </DevSkill>
+          <DevSkill>
+            <i className="fab fa-codepen"></i> Codepen{" "}
+            <i className="fab fa-codepen"></i>
+          </DevSkill>
+          <DevSkill>
+            <i className="fas fa-code-branch"></i> Github Pages{" "}
+            <i className="fas fa-code-branch"></i>
+          </DevSkill>
+          <DevSkill>
+            <i className="far fa-image"></i> Adobe Photoshop{" "}
+            <i className="fas fa-image"></i>
+          </DevSkill>
+          <DevSkill>
+            <i className="fa-brands fa-figma"></i> Figma{" "}
+            <i className="fa-brands fa-figma"></i>
+          </DevSkill>
+        </DevSkills>
+      </Wrapper>
     </Container>
   );
 }
--- src/Components/Skills/SkillsStyles.js
+++ src/Components/Skills/SkillsStyles.js
@@ -1,6 +1,80 @@
 import styled from "styled-components";
 import { tablet, desktop } from "../../responsive";
 
-const Container = styled.section``;
+const Container = styled.section`
+  background-color: var(--main-BG);
+  height: 100vh;
+  padding: 7rem 0;
+`;
 
-export { Container };
+const Wrapper = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`;
+
+const Header = styled.h2`
+  letter-spacing: 2px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: var(--BrighterText);
+  text-shadow: var(--headerTextShadow);
+
+  &::after {
+    content: "";
+    height: 1px;
+    width: 100%;
+    margin-left: 15px;
+    background-color: var(--mainTextColor);
+    box-shadow: 3px 3px 2px var(--highlightColor);
+
+    ${tablet({
+      width: "300px",
+    })}
+
+    ${desktop({
+      width: "500px",
+    })}
+  }
+
+  ${tablet({
+    fontSize: "1.7rem",
+    justifyContent: "left",
+  })}
+`;
+
+const DevSkills = styled.div`
+  display: flex;
+  flex-flow: wrap;
+  margin: 2rem;
+  gap: 2rem;
+  justify-content: center;
+  align-items: center;
+
+  ${tablet({
+    width: "80%",
+  })}
+`;
+
+const DevSkill = styled.p`
+  flex: 1;
+  min-width: min-content;
+  white-space: nowrap;
+  text-align: center;
+  padding: 15px 20px;
+
+  text-transform: uppercase;
+  letter-spacing: 1px;
+
+  border: 1px solid var(--highlightColor);
+  color: var(--dimHighlightColor);
+  transition: all 0.5s ease-in-out;
+
+  &:hover {
+    color: var(--highlightColor);
+    border: 1px solid var(--dimHighlightColor);
+    border-radius: 15px;
+  }
+`;
+
+export { Container, Wrapper, Header, DevSkills, DevSkill };
