06031c93f06cd591ab430460dc475ebe217ce02f
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-03-01 15:04:57 -0500

    Nav CSS Replaced with Styled Comp

--- src/Components/Navbar/Nav.js
+++ src/Components/Navbar/Nav.js
@@ -1,15 +1,23 @@
 import {
+  CloseBtn,
   Container,
   DropdownBtn,
   DropdownContent,
   HomeBtn,
+  Link,
   Logo,
   Navigation,
+  NavItem,
+  NavList,
+  Resume,
+  ResumeLink,
+  Wrapper,
 } from "./NavStyles";
 
 function Nav({ logo }) {
   let prevScroll = window.pageYOffset;
-  let dropdown = document.getElementById("dd-content");
+  const dropdown = document.getElementById("dd-content");
+  const navbar = document.getElementById("nava");
 
   function showHide() {
     dropdown.style.width = "40%";
@@ -28,57 +36,62 @@ function Nav({ logo }) {
   window.onscroll = function () {
     let currentPos = window.pageYOffset;
     if (prevScroll > currentPos) {
-      document.getElementById("nava").style.top = "0";
+      navbar.style.top = "0";
     } else {
-      document.getElementById("nava").style.top = "-1000px";
+      navbar.style.top = "-1000px";
     }
     prevScroll = currentPos;
   };
 
   return (
     <Container id="nava">
-      <HomeBtn href="">
-        <Logo id="logo" src={logo} />
-      </HomeBtn>
-      <Navigation>
-        <DropdownBtn
-          onClick={showHide}
-          className="fas fa-soild fa-bars"
-        ></DropdownBtn>
-        <DropdownContent id="dd-content" className="dropdown-content">
-          <ul>
-            <li className="nav-item close-button">
-              {" "}
-              <a onClick={closeBtn}>Close</a>
-            </li>
-            <li className="nav-item">
-              <a href="#skills">
-                <i className="fas fa-tasks"></i> Skills
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#projects">
-                <i className="fas fa-code"></i> Projects
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#about">
-                <i className="fas fa-user"></i> About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#contact">
-                <i className="fa-solid fa-address-card"></i> Contact
-              </a>
-            </li>
-            <li className="nav-item resume-nav">
-              <a href={require("../../Resume/resume-file.pdf")} target="_blank">
-                <i className="fa-solid fa-file"></i> Resume
-              </a>
-            </li>
-          </ul>
-        </DropdownContent>
-      </Navigation>
+      <Wrapper>
+        <HomeBtn href="">
+          <Logo id="logo" src={logo} />
+        </HomeBtn>
+        <Navigation>
+          <DropdownBtn
+            onClick={showHide}
+            className="fas fa-soild fa-bars"
+          ></DropdownBtn>
+          <DropdownContent id="dd-content">
+            <NavList>
+              <CloseBtn>
+                {" "}
+                <p onClick={closeBtn}>Close</p>
+              </CloseBtn>
+              <NavItem>
+                <Link href="#skills">
+                  <i className="fas fa-tasks"></i> Skills
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link href="#projects">
+                  <i className="fas fa-code"></i> Projects
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link href="#about">
+                  <i className="fas fa-user"></i> About
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link href="#contact">
+                  <i className="fa-solid fa-address-card"></i> Contact
+                </Link>
+              </NavItem>
+              <Resume>
+                <ResumeLink
+                  href={require("../../Resume/resume-file.pdf")}
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-file"></i> Resume
+                </ResumeLink>
+              </Resume>
+            </NavList>
+          </DropdownContent>
+        </Navigation>
+      </Wrapper>
     </Container>
   );
 }
--- src/Components/Navbar/NavStyles.jsx
+++ src/Components/Navbar/NavStyles.jsx
@@ -2,10 +2,6 @@ import styled from "styled-components";
 import { desktop, tablet } from "../../responsive";
 
 const Container = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
   background: linear-gradient(
     0deg,
     rgba(0, 0, 0, 0) 25%,
@@ -27,6 +23,12 @@ const Container = styled.nav`
   ${tablet({
     padding: "20px 40px",
   })}
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const HomeBtn = styled.a``;
@@ -63,6 +65,95 @@ const DropdownContent = styled.div`
 
   background-color: rgb(5, 5, 5);
   transition: width 1s ease-in-out;
+
+  ${tablet({
+    display: "block",
+    position: "static",
+    backgroundColor: "unset",
+    height: "unset",
+    width: "unset",
+  })}
 `;
 
-export { Container, HomeBtn, Logo, Navigation, DropdownBtn, DropdownContent };
+const NavList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  gap: 2rem;
+
+  ${tablet({
+    flexDirection: "row",
+    backgroundColor: "unset",
+  })}
+`;
+
+const CloseBtn = styled.li`
+  text-align: right;
+  padding-bottom: 3rem;
+  transition: all 0.5;
+  cursor: pointer;
+  padding: 1.4em;
+  font-size: 1.2rem;
+
+  ${tablet({
+    display: "none",
+  })}
+`;
+
+const NavItem = styled.li`
+  font-size: 1.2rem;
+  padding: 1.2rem 1rem;
+  white-space: nowrap;
+  word-spacing: 2px;
+
+  ${tablet({
+    padding: "1rem",
+    fontSize: "unset",
+  })}
+`;
+
+const Link = styled.a`
+  transition: all 0.5s;
+  &:hover {
+    color: var(--highlightColor);
+  }
+`;
+
+const Resume = styled.a`
+  font-size: 1.2rem;
+  padding: 1.2rem 1rem;
+  white-space: nowrap;
+  word-spacing: 2px;
+
+  ${tablet({
+    padding: "1rem",
+    fontSize: "unset",
+  })}
+`;
+
+const ResumeLink = styled.a`
+  ${tablet({
+    border: "1px solid var(--highlightColor)",
+    color: "var(--highlightColor)",
+    padding: "1rem 1.3rem",
+    borderRadius: "5px",
+    transition: "all .5s",
+  })}
+`;
+
+export {
+  Container,
+  Wrapper,
+  HomeBtn,
+  Logo,
+  Navigation,
+  DropdownBtn,
+  DropdownContent,
+  NavList,
+  CloseBtn,
+  NavItem,
+  Link,
+  Resume,
+  ResumeLink,
+};
