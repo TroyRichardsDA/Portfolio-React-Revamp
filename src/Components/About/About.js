ea0304a0b1464c1a9cf8be4489ab8152ee3ba3f7
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-06-21 15:26:37 -0400

    About Section Redesigned

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,30 @@
-import { Container, Header, Text, TextContainer, Wrapper } from "./AboutStyles";
+import {
+  Container,
+  Header,
+  MobileShowMore,
+  ShowMore,
+  Text,
+  TextContainer,
+  ToggleShowMore,
+  Wrapper,
+} from "./AboutStyles";
+import { FaRegLightbulb } from "react-icons/fa";
+import { HiLightBulb } from "react-icons/hi";
+import { useState } from "react";
 
 function About() {
+  const [showMore, setShowMore] = useState(false);
+
+  function toggleShowMore() {
+    if (showMore) {
+      setShowMore(false);
+      document.getElementById("about").classList.remove("show-more");
+    } else {
+      setShowMore(true);
+      document.getElementById("about").classList += " show-more";
+    }
+  }
+
   return (
     <Container id="about">
       <Wrapper
@@ -10,56 +34,56 @@ function About() {
         viewport={{ once: true }}
       >
         <Header>
-          <b className="important-text">A</b>bout
+          <b className="important-text section__title">A</b>bout me
         </Header>
 
+        <ToggleShowMore onClick={() => toggleShowMore()}>
+          Wanna Know More? <HiLightBulb className="light-bulb--lit" />
+          <FaRegLightbulb className="light-bulb--out" />
+        </ToggleShowMore>
+
         <TextContainer>
-          <Text>
-            Hello! My name is Troy Richards, and I am, what some would call, a{" "}
-            <br /> <span className="shake">🎉</span>{" "}
-            <b className="skills-used about-title">Frontend Developer!!!!!!</b>{" "}
-            <span className="shake">🎉</span>
-            <br />
-            <small>
-              if they don't call me that, they don't know about me. Don't worry
-              about them, we're talking about me.
-            </small>
-          </Text>
+          <div>
+            <Text>
+              Hello! My name is Troy Richards, and I am, what some would call, a{" "}
+              <b className="important-text">Frontend Developer!!!!!!</b>
+            </Text>
+            <Text>
+              If you couldn't tell from the AMAZING animations so far, I love to
+              create websites/apps that look and feel amazing to use. I mainly
+              work with <span className="skills-used">react</span> and{" "}
+              <span className="skills-used">sass</span> but don't mind branching
+              out for the right opportunity.
+            </Text>
+            <Text>
+              <span className="important-text">Currently</span>, I am looking
+              for opportunities to work remotely as a{" "}
+              <span className="important-text"> Frontend Developer</span>. Want
+              to know what I can bring to your team? Just a bit more scrolling
+              and you can{" "}
+              <a href="#skills" className="important-text-link">
+                find out!
+              </a>
+            </Text>
+          </div>
 
-          <Text>
-            If you couldn't tell from the AMAZING animations so far, I love to
-            create websites/apps that look and feel amazing to use. I mainly
-            work with <span className="skills-used">react</span> and{" "}
-            <span className="skills-used">sass</span> but don't mind branching
-            out for the right opportunity.
-          </Text>
-
-          <Text>
-            <b className="important-text">
-              You can skip this chunk if you're pressed for time,
-            </b>{" "}
-            but I would be happy if you read it. Let's talk about me as a
-            person. I like to play games in my spare time, mainly moba's
-            {"(SMITE!!!)"} and my favorite role is jungle. Don't ever ask me to
-            play support. Just don't. Another thing about me is I use to make
-            use of my degree in audio production and make music. However, when
-            covid hit, I had to sell a lot of my equipment and that made me very
-            sad :(. My dream is to be able to buy my equipment back{" "}
-            {"(over even better equipment)"} and go back to producing in my
-            spare time. But enough about that, let's move on to what you're
-            really here for!
-          </Text>
-
-          <Text>
-            <span className="important-text">Currently</span>, I am looking for
-            opportunities to work remotely as a{" "}
-            <span className="important-text"> Frontend Developer</span>. Want to
-            know what I can bring to your team? Just a bit more scrolling and
-            you can{" "}
-            <a href="#skills" className="important-text-link">
-              find out!
-            </a>
-          </Text>
+          <ShowMore>
+            <MobileShowMore onClick={() => toggleShowMore()}>
+              Wanna Know More? <HiLightBulb className="light-bulb--lit" />
+              <FaRegLightbulb className="light-bulb--out" />
+            </MobileShowMore>
+            <p className="show-more__para">
+              Let's talk about me as a person. I like to play games in my spare
+              time, mainly moba's
+              {"(SMITE!!!)"} and my favorite role is jungle. Don't ever ask me
+              to play support. Just don't. Wrong tree. Another thing about me is
+              I use to make use of my degree in audio production and make music.
+              However, when covid hit, I had to sell a lot of my equipment and
+              that made me very sad :(. My goal is buy my equipment back{" "}
+              {"(or even better equipment)"} and get back to producing in my
+              spare time.
+            </p>
+          </ShowMore>
         </TextContainer>
       </Wrapper>
     </Container>
--- src/Components/About/AboutStyles.js
+++ src/Components/About/AboutStyles.js
@@ -3,12 +3,13 @@ import { tablet, desktop } from "../../responsive";
 import { motion } from "framer-motion";
 
 export const Container = styled.section`
-  padding-top: 10rem;
-  padding-bottom: 10rem;
+  padding: 40px 0;
 
   background-color: var(--main-BG);
   border-top: 4px solid var(--highlightColor);
   border-bottom: 4px solid var(--highlightColor);
+
+  min-height: 100vh;
 `;
 
 export const Wrapper = styled(motion.div)`
@@ -16,25 +17,37 @@ export const Wrapper = styled(motion.div)`
   margin: 0 auto;
 
   text-align: center;
+
+  ${tablet({
+    textAlign: "unset",
+  })}
 `;
 
 export const Header = styled.h2`
-  letter-spacing: 2px;
+  margin: 20px 0 40px;
   font-size: var(--headers);
-`;
-
-export const SubHeader = styled.h1`
-  color: var(--highlightColor);
-  font-size: 3rem;
-  margin: 3rem 0;
+  letter-spacing: 4px;
+  position: relative;
 
   ${tablet({
-    fontSize: "3rem",
+    textAlign: "center",
   })}
+`;
 
-  ${desktop({
-    fontSize: "3rem",
-    lineHeight: "20px",
+export const ToggleShowMore = styled.h3`
+  display: none;
+  position: relative;
+  cursor: pointer;
+
+  &:hover {
+    color: var(--dimHighlightColor);
+  }
+
+  ${tablet({
+    textAlign: "center",
+    display: "block",
+    marginBottom: "20px",
+    fontSize: "20px",
   })}
 `;
 
@@ -46,15 +59,16 @@ export const Small = styled.span`
 
 export const TextContainer = styled.div`
   display: grid;
-  gap: 1.5rem;
+  gap: 20px;
   line-height: 2rem;
 
   ${tablet({
     gap: "2.4rem",
+    gridTemplateColumns: "1fr 1fr",
   })}
 
   ${desktop({
-    width: " 70%",
+    width: "90%",
     margin: "0 auto",
     fontSize: "1.2rem",
     lineHeight: "2rem",
@@ -62,7 +76,8 @@ export const TextContainer = styled.div`
 `;
 
 export const Text = styled.p`
-  transition: 0.3s;
+  letter-spacing: 2px;
+  margin-bottom: 12px;
 
   .about-title {
     display: inline-block;
@@ -82,4 +97,16 @@ export const Text = styled.p`
   &:hover {
     opacity: 1 !important;
   }
+`;
+
+export const ShowMore = styled.div`
+  h3 {
+  }
+`;
+
+export const MobileShowMore = styled(ToggleShowMore)`
+  display: block;
+  ${tablet({
+    display: "none",
+  })}
 `;
