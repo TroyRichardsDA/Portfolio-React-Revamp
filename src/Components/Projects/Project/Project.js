c57965a5f93e143208c94b350a44e27315ad8f39
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-07-31 16:45:14 -0400

    Translated Projects from styled-componets to sass

--- src/Components/Projects/Project/Project.js
+++ src/Components/Projects/Project/Project.js
@@ -1,55 +1,37 @@
-import {
-  Container,
-  Desc,
-  DevWith,
-  Image,
-  ImageCover,
-  ImgContainer,
-  Link,
-  ProjectLinks,
-  TextContainer,
-  Title,
-} from "./ProjectStyles";
 import { AiOutlineGithub, AiOutlineLink } from "react-icons/ai";
+import { motion } from "framer-motion";
 
-export default function Project({
-  title,
-  liveSite,
-  sourceCode,
-  desc,
-  devWith,
-  img,
-  x,
-  y,
-}) {
+export default function Project(props) {
+  const { title, liveSite, sourceCode, desc, devWith, img, x, y } = props;
+
   return (
-    <Container
+    <motion.div
       initial={{ x, y, opacity: 0 }}
       whileInView={{ x: 0, y: 0, opacity: 1 }}
       viewport={{ once: true }}
       transition={{ duration: 2 }}
     >
-      <ImgContainer>
-        <Image src={img} alt={title} />
+      <div className="project__img--container">
+        <img className="project__img" src={img} alt={title} />
 
-        <ImageCover>
-          <TextContainer>
-            <Title>{title}</Title>
-            <DevWith>{devWith}</DevWith>
-            <Desc>
-              {desc}
-              <ProjectLinks>
-                <Link href={liveSite} target="_blank">
+        <div className="project__img--cover">
+          <div className="project__text">
+            <h3 className="project__text--title">{title}</h3>
+            <p className="project__text--tech">{devWith}</p>
+            <div className="project__desc">
+              <p>{desc}</p>
+              <div className="project__links">
+                <p className="project__link" href={liveSite} target="_blank">
                   <AiOutlineLink />
-                </Link>
-                <Link href={sourceCode} target="_blank">
+                </p>
+                <p className="project__link" href={sourceCode} target="_blank">
                   <AiOutlineGithub />
-                </Link>
-              </ProjectLinks>
-            </Desc>
-          </TextContainer>
-        </ImageCover>
-      </ImgContainer>
-    </Container>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.div>
   );
 }
